e325b7455fe9903dbc8107d03f7dc07ffa60a3d6
Stefan Eichenberger <eichest@gmail.com>
2016-04-16 13:22:52 +0200

    engine.py: don't use SessionHook
    Commit 6655ced441ebc7a8ebad3649e5962a238bea20c3 of neovim python-client
    removed the SessionHook because of this engine.py fails if it tries to
    access nvim.session. This patch fixes this issue.
    
    Signed-off-by: Stefan Eichenberger <eichest@gmail.com>

--- python/engine.py
+++ python/engine.py
@@ -104,7 +104,7 @@ def engine_start():
 
     unsaved = []
     while (_is_running):
-        event = nvim.session.next_message()
+        event = nvim.next_message()
         if not event:
             continue
 
@@ -182,13 +182,13 @@ def engine_start():
 
             result = {'cursor':str(cursor), 'cursor.kind': str(cursor.kind), 'cursor.type.kind': str(cursor.type.kind), 'cursor.spelling' : cursor.spelling}
             event[3].send(result)
-            
+
         elif event[1] == 'update_unsaved_all':
             _update_unsaved_all(nvim, unsaved)
             event[3].send('ok')
 
         elif event[1] == 'shutdown':
-            nvim.session.stop()
+            nvim.stop()
             _is_running = False
             event[3].send('ok')
 
